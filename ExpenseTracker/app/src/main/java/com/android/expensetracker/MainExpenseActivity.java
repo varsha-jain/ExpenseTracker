bd29fab4e9183ccda39d2027e5d02c8ae1e27ccc
Varsha Jain <Varsha Jain>
2017-07-08 23:37:24 -0400

    add

--- ExpenseTracker/app/src/main/java/com/android/expensetracker/MainExpenseActivity.java
+++ ExpenseTracker/app/src/main/java/com/android/expensetracker/MainExpenseActivity.java
@@ -5,12 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class MainExpenseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_expense);
+
     }
 
     public void addExpense(View view){
@@ -19,7 +24,23 @@ public class MainExpenseActivity extends AppCompatActivity {
     }
 
     public void viewExpense(View view){
+        Intent i = getIntent();
+        HashMap<Integer, HashMap<Integer, Float>> map = (HashMap<Integer, HashMap<Integer, Float>>) i.getSerializableExtra("EXTRA_MESSAGE");
+
+        Iterator itr = map.entrySet().iterator();
+        while(itr.hasNext()){
+            Map.Entry p = (Map.Entry)itr.next();
+            System.out.println("year;"+p.getKey());
+            HashMap<Integer, Float> m1 = map.get(p.getKey());
+            Iterator it = m1.entrySet().iterator();
+            while(it.hasNext()){
+                Map.Entry p1 = (Map.Entry)it.next();
+                System.out.println("month:" + p1.getKey()+"  "+ "total:" + p1.getValue());
+            }
+        }
+        //
         Intent intent = new Intent(this, ViewExpense.class);
+        intent.putExtra("HashMap", map);
         startActivity(intent);
     }
 }
--- ExpenseTracker/app/src/main/java/com/android/expensetracker/ViewExpense.java
+++ ExpenseTracker/app/src/main/java/com/android/expensetracker/ViewExpense.java
@@ -5,13 +5,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class ViewExpense extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_expense);
-        Toast.makeText(this,"hello",Toast.LENGTH_SHORT);
-
+        Toast.makeText(this,"hello",Toast.LENGTH_SHORT).show();
+        Intent i = getIntent();
+        HashMap<Integer, HashMap<Integer, Float>> map = (HashMap<Integer, HashMap<Integer, Float>>) i.getSerializableExtra("HashMap");
+        Iterator itr = map.entrySet().iterator();
+        while(itr.hasNext()){
+            Map.Entry p = (Map.Entry)itr.next();
+            System.out.println("Inside view expense");
+            System.out.println("year;"+p.getKey());
+            HashMap<Integer, Float> m1 = map.get(p.getKey());
+            Iterator i1 = m1.entrySet().iterator();
+            while(i1.hasNext()){
+                Map.Entry p1 = (Map.Entry)i1.next();
+                System.out.println("month:" + p1.getKey()+"  "+ "total:" + p1.getValue());
+            }
+        }
     }
 }
